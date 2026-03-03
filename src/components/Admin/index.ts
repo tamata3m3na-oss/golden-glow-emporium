87eae276f9ea2a9f9a6849fcb6550e8e3829f5c8
tamata3m3na-oss <tamata3m3na@gmail.com>
2026-03-03 09:58:25 +0300

    Delete src directory

--- src/components/Admin/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as ProductForm } from './ProductForm';
-export { default as ProductsTab } from './ProductsTab';
-export { default as OrdersTab } from './OrdersTab';
-export { default as StatsTab } from './StatsTab';
-export * from './types';
